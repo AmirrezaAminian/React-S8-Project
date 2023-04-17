18a7c1b31f5cce595b836728d25bf99ef0ed74bf
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-04-18 03:25:24 +0430

    Adding Calidations & Resetting Logic

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -9,22 +9,41 @@ const AddUser = (props) => {
 
   const addUserHandler = (e) => {
     e.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+
+    if (+enteredAge < 1) {
+      return;
+    }
+    setEnteredUsername("");
+    setEnteredAge("");
   };
 
   const usernameChangeHandler = (e) => {
-    setEnteredUsername(e.target.value)
-  }
+    setEnteredUsername(e.target.value);
+  };
   const ageChangeHandler = (e) => {
-    setEnteredAge(e.target.value)
-  }
+    setEnteredAge(e.target.value);
+  };
 
   return (
     <Card className={clasess.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler}></input>
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        ></input>
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" onChange={ageChangeHandler}></input>
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        ></input>
         <Button type="submit">Add User</Button>
       </form>
     </Card>
